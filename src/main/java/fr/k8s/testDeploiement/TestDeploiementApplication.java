48382697b41a28ad34643c33b99ef4b87228be7f
Anthony Couture <antobox28@gmail.com>
2020-09-30 21:42:38 +0200

    git init

---
+++ src/main/java/fr/k8s/testDeploiement/TestDeploiementApplication.java
@@ -0,0 +1,13 @@
+package fr.k8s.testDeploiement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDeploiementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDeploiementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/k8s/testDeploiement/TestDeploiementApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.k8s.testDeploiement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDeploiementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
